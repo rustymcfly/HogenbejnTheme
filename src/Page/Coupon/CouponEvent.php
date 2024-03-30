09d6ebdf11b1a833f34d10a9505b7bfd969bbac8
mflog <flogaus@die-leoparden.de>
2024-03-30 20:26:46 +0100

    fix CouponEvent

--- src/Page/Coupon/CouponEvent.php
+++ src/Page/Coupon/CouponEvent.php
@@ -5,15 +5,15 @@ namespace RustyMcFly\HogenbejnTheme\Page\Coupon;
 use Monolog\Logger;
 use Shopware\Core\Content\Flow\Dispatching\Aware\DataAware;
 use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\Event\BusinessEventInterface;
 use Shopware\Core\Framework\Event\EventData\ArrayType;
 use Shopware\Core\Framework\Event\EventData\EventDataCollection;
 use Shopware\Core\Framework\Event\EventData\ObjectType;
 use Shopware\Core\Framework\Event\EventData\ScalarValueType;
+use Shopware\Core\Framework\Event\FlowEventAware;
 use Shopware\Core\Framework\Log\LogAware;
 use Symfony\Contracts\EventDispatcher\Event;
 
-class CouponEvent extends Event implements BusinessEventInterface, LogAware, DataAware
+class CouponEvent extends Event implements FlowEventAware, LogAware, DataAware
 {
 
     const EVENT_NAME = 'coupon.event';
